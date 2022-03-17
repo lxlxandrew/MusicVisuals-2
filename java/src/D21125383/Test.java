6b8f479ee67208318d22d19b67d25c85e889ce1a
Amrawsome <d21125383@mytudublin.ie>
2022-03-17 12:18:34 +0000

    start

---
+++ java/src/D21125383/Test.java
@@ -0,0 +1,28 @@
+package D21125383;
+
+import processing.core.PApplet;
+
+public class Test extends PApplet{
+
+public void settings(){
+size(500, 500);
+
+}
+
+public void setup(){
+background(0);
+noFill();
+}
+
+public void draw(){
+stroke(255); 
+
+circle(250, 250,200);
+textSize(50);
+
+text("Hello", 190, 270);
+
+
+}
+
+}
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,5 +1,5 @@
 package ie.tudublin;
-
+import D21125383.Test;
 import example.CubeVisual;
 import example.MyVisual;
 import example.RotatingAudioBands;
@@ -13,9 +13,16 @@ public class Main
         processing.core.PApplet.runSketch( a, new MyVisual());		
 	}
 
+	public void Test()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Test());		
+	}
 	public static void main(String[] args)
 	{
 		Main main = new Main();
-		main.startUI();			
+		main.Test();			
 	}
+
+	
 }
